09d58e858da6309cd6a55636b91707b3d546850d
craigthelinguist <aaroncraig@protonmail.ch>
2015-10-10 16:41:28 +1300

    Encapsulate parsing variables inside a struct.

--- parser.c
+++ parser.c
@@ -100,148 +100,148 @@ struct stmt {
    };
 };
 
+struct parse_data {
+   char *text;
+   int index;
+   int len;
+   int line_num;
+};
+
+typedef struct parse_data DATA;
+
 
 
 // Function definitions.
 // ======================================================================
 
 // Parsing helpers.
-static inline int done ();
-static inline void skip_whitespace ();
-static inline void err (char *msg);
-static inline Str *parse_string ();
-static inline int gobble_char (char c);
-static inline int gobble_str (char *s);
+static inline int done (DATA data);
+static inline void skip_whitespace (DATA data);
+static inline void err (DATA data, char *msg);
+static inline Str *parse_string (DATA data);
+static inline int gobble_char (DATA data, char c);
+static inline int gobble_str (DATA data, char *s);
 static inline void validate_program (struct program *program);
 
 // Parsing grammar rules.
-static inline Str *Parse_String();
-static inline Str *Parse_Name();
-static inline int Parse_Inputs();
-static inline Str *Parse_InitState();
-static inline struct state Parse_State();
-static inline int Count_States();
-
-
-// Variables.
-// ======================================================================
-
-static int index = 0;
-static char *data = 0;
-static int len = 0;
-static int line_num = 1;
+static inline Str *Parse_String(DATA);
+static inline Str *Parse_Name(DATA);
+static inline int Parse_Inputs(DATA);
+static inline Str *Parse_InitState(DATA);
+static inline struct state Parse_State(DATA);
+static inline int Count_States(DATA);
 
 
 
 // Parsing helpers.
 // ======================================================================
 
-static inline int done()
+static inline int done(DATA data)
 {
-   skip_whitespace();
-   return index >= len;
+   skip_whitespace(data);
+   return data.index >= data.len;
 }
 
-static inline void skip_whitespace ()
+static inline void skip_whitespace (DATA data)
 {
    int i = 0;
-   while (index < len && isspace(data[index+i])) {
-      char c = data[index+i];
+   while (data.index < data.len && isspace(data.text[data.index+i])) {
+      char c = data.text[data.index+i];
       if (c == '\r' || c == '\n')
-         line_num++;
+         data.line_num++;
       i++;
    }
 }
 
-static inline void err(char *msg)
+static inline void err (DATA data, char *msg)
 {
-   fprintf(stderr, "Line %d: %s\n", line_num, msg);
+   fprintf(stderr, "Line %d: %s\n", data.line_num, msg);
    abort();
 }
 
-static inline Str *Parse_String ()
+static inline Str *Parse_String (DATA data)
 {
    int i = 0;
-   while (!isalpha(data[index+i])) i++;
+   while (!isalpha(data.text[data.index+i])) i++;
    char contents[i];
-   memcpy(contents, data + index, i);
-   index += i;
+   memcpy(contents, data.text + data.index, i);
+   data.index += i;
    return Str_Make(contents);
 }
 
-static inline int gobble_char (char c)
+static inline int gobble_char (DATA data, char c)
 {
-   if (data[index] != c) return 0;;
-   index++;
+   if (data.text[data.index] != c) return 0;;
+   data.index++;
    return 1;
 }
 
-static inline int gobble_str (char *s)
+static inline int gobble_str (DATA data, char *s)
 {
    int i;
-   for (i=0; s[i] != '\0' && isalnum(data[index+i]); i++) {
-      if (s[i] != data[index+i]) return 0;
+   for (i=0; s[i] != '\0' && isalnum(data.text[data.index+i]); i++) {
+      if (s[i] != data.text[data.index+i]) return 0;
    }
-   if (isalnum(data[index+i])) return 0;
-   index += i;
+   if (isalnum(data.text[data.index+i])) return 0;
+   data.index += i;
    return 1;
 }
 
-static inline int gobble_str_sensitive (char *s)
+static inline int gobble_str_sensitive (DATA data, char *s)
 {
    int i;
-   for (i=0; s[i] != '\0' && isalnum(data[index+i]); i++) {
-      if (tolower(s[i]) != tolower(data[index+i])) return 0;
+   for (i=0; s[i] != '\0' && isalnum(data.text[data.index+i]); i++) {
+      if (tolower(s[i]) != tolower(data.text[data.index+i])) return 0;
    }
-   if (isalnum(data[index+i])) return 0;
-   index += i;
+   if (isalnum(data.text[data.index+i])) return 0;
+   data.index += i;
    return 1;
 }
 
 static inline void validate_program (struct program *program)
 {
    if (program->num_inputs < 0)
-      err("%s has %d inputs - should have a non-negative amount.");//, program->name, program->num_inputs);
+      fprintf(stderr, "Program has %d inputs - should have a non-negative amount.", program->num_inputs);
    if (!Map_Contains(program->states, program->init_state))
-      err("initial state of %s is %s, which hasn't been defined.");//, program->name, program->init_state);
+      fprintf(stderr, "Initial state is %s, which hasn't been defined.", Str_Guts(program->init_state));
 }
 
 
 // Parsing grammar rules.
 // ======================================================================
 
-static inline Str *Parse_Name()
+static inline Str *Parse_Name(DATA data)
 {
-   if (!gobble_str_insensitive("Name"))
-      err("Expected name declaration.");
-   skip_whitespace();
-   if (!gobble_char(':'))
-      err("Missing ':' in name declaration.");
-   skip_whitespace();
-   Str *name = Parse_String();
-   if (!gobble_char('.'))
-      err("Missing '.' terminator in name declaration (Name:%s)");//, name);
-   skip_whitespace();
+   if (!gobble_str_insensitive(data, "Name"))
+      err(data, "Expected name declaration.");
+   skip_whitespace(data);
+   if (!gobble_char(data, ':'))
+      err(data, "Missing ':' in name declaration.");
+   skip_whitespace(data);
+   Str *name = Parse_String(data);
+   if (!gobble_char(data, '.'))
+      err(data, "Missing '.' terminator in name declaration (Name:%s)");//, name);
+   skip_whitespace(data);
    return name;
 }
 
-static inline int Parse_Inputs()
+static inline int Parse_Inputs(DATA data)
 {
    return -1;
 }
 
-static inline Str *Parse_InitState()
+static inline Str *Parse_InitState(DATA data)
 {
    return NULL;
 }
 
-static inline struct state Parse_State()
+static inline struct state Parse_State(DATA data)
 {
    struct state state;
    return state;
 }
 
-static inline int Count_States()
+static inline int Count_States(DATA data)
 {
    return -1;
 }
@@ -268,34 +268,36 @@ struct program *Parse_Program (Str *string)
 {
 
    // Ready the parser.
-   index = 0;
-   len = Str_Len(string);
-   data = Str_Guts(string);
+   struct parse_data data;
+   data.index = 0;
+   data.len = Str_Len(string);
+   data.text = Str_Guts(string);
+   data.line_num = 1;
 
    // Parse meta info.
-   Str *routine_name = Parse_Name();
-   int num_inputs = Parse_Inputs();
+   Str *routine_name = Parse_Name(data);
+   int num_inputs = Parse_Inputs(data);
    if (num_inputs < 0)
-      err("Must have non-negative number of inputs.");
-   Str *init_state = Parse_InitState();
+      err(data, "Must have non-negative number of inputs.");
+   Str *init_state = Parse_InitState(data);
 
    // Create and initialise the program.
    struct program *program = malloc(sizeof program);
    program->num_inputs = num_inputs;
    program->init_state = init_state;
    program->name = routine_name;
-   int num_states = Count_States();
+   int num_states = Count_States(data);
    if (num_states == 0)
-      err("Need at least one state.");
+      err(data, "Need at least one state.");
    program->states = Map_Make(num_states, sizeof(Str *), sizeof(struct state), NULL,
                               Map_FreeStr, Map_CmpStr, NULL, NULL);
 
    // Parse definitions.
    int i;
    for (i=0; i < num_states; i++) {
-      struct state state = Parse_State();
+      struct state state = Parse_State(data);
       if (Map_Contains(program->states, state.name))
-         err("state being defined again.");
+         err(data, "state being defined again.");
       Map_Put(program->states, state.name, &state);
    }
 
